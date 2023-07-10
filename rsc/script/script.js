46e1aed0330314c4399dd1ce650977087e74b5c9
Julie Le Scao <lescao.julie@gmail.com>
2023-07-10 18:23:53 +0200

    fonction aff temp min moy max sun ico next et saint

--- rsc/script/script.js
+++ rsc/script/script.js
@@ -1,6 +1,6 @@
 // données et fonction hors API
 // recupération de la date, de l'heure et mise à jour toutes les secondes
-const dateInterval = setInterval(newDate, newHour, 1000); 
+const dateInterval = setInterval(majDateHeure, 1000); 
 newDate(); // premier affichage
 newHour(); // premier affichage
 function newDate() {
@@ -21,35 +21,138 @@ function newHour() {
 
     const hourDiv = document.getElementById("hour");
     hourDiv.textContent = hours + ":" + mins + ":" + secs;
-}
+};
+function majDateHeure() {
+    newDate();
+    newHour();
+};
 
-// données recupérées en json pour les saints de l'API
-function fetchNasa(evt) {
-    const uri = `https://api.nasa.gov/planetary/apod?api_key=xWLKZjY1HYy6lRpbsSaQJ7W6DrULd15xOuldiwSJ&date=${evt.target.value}`;
-    fetch(uri) // Promesse d'aller collecter les données à l'URI fournie
+
+// données recupérées en json pour les saints de l'API de nominis
+fetchSaint(); // on affiche le saint du jour
+function fetchSaint() {
+    const uri = `https://nominis.cef.fr/json/saintdujour.php`;
+    fetch(uri) 
         .then((resp) => {
-            if (resp.ok) { // Les données collectées sont valides
-                return resp.json(); // Promesse d'extirper les données de la réponse
+            if (resp.ok) { 
+                return resp.json();
             } else {
-                throw new Error(`Données non collectées avec l'erreur ${resp.status} : ${resp.statusText}`);
+                throw new Error(`Les données n'ont pu être collectées. ${resp.status} : ${resp.statusText}`);
             }
         })
         .then((data) => {
-            const out = document.getElementById("out");
-            out.innerHTML = `
-            <figure>
-                <div>
-                    <img src="${data.url}" alt="${data.title}">
-                </div>
-                <figcaption>${data.explanation}</figcaption>
-            </figure>`;
+            const saint = document.getElementById("saint");
+            saint.textContent = data.response.saintdujour.nom;
         })
         .catch((err) => {
             throw new Error(`Un problème réseau est survenu (${err}).`);
         });
 }
 
+// données recupérées en json pour la méteo de l'API de https://www.prevision-meteo.ch/services/json/toulouse
+//on recupère les données du site
+async function fetchJsonWeatherData(uri) {
+    const resp = await fetch(uri); // Mets le script en attente de la data
+    let data; // On prépare la variable à utiliser
+    if (resp.ok) data = await resp.json(); // Mets le script en attente du "décryptage" de la data
+    else throw new Error("données non collectées"); // Gestion des erreurs
+    return data; // On retourne la donnée "pure"
+}        
+// on mets à jour les fonctions pour l'affichage de l'icone du temps et de la temperature à jour toutes les heures
+const weatherInterval = setInterval(majIcoTemp, 3600000);
+newIco(); // affichage de l'icone du tempsqu'il fait
+newTemp(); // affichage de la température
+newSun(); // affichage du levé et du couché du soleil
+newTown(); // affichage de la ville
+newMinMoyMax(); // affichage minimale, moyenne et maximale du jour
+newNextDay();
+function majIcoTemp() {
+newIco();
+newTemp();
+};
+
+async function newIco() {
+    const uri = `https://www.prevision-meteo.ch/services/json/toulouse`;
+    try { 
+        const data = await fetchJsonWeatherData(uri);
+        const ico = document.getElementById("ico");
+        ico.src = data.current_condition.icon;
+    } catch (err) { // Si une erreur est survenue dans le code précédent je la traite ici
+        console.error(err);
+    }
+};
+async function newTemp() {
+    const uri = `https://www.prevision-meteo.ch/services/json/toulouse`;
+    try { 
+        const data = await fetchJsonWeatherData(uri);
+        const temp = document.getElementById("currentTemp");
+        temp.textContent = `${data.current_condition.tmp}°`;
+    } catch (err) { 
+        console.error(err);
+    }
+};
+async function newSun() {
+    const uri = `https://www.prevision-meteo.ch/services/json/toulouse`;
+    try { 
+        const data = await fetchJsonWeatherData(uri);
+        const sunrise = document.getElementById("sunrise");
+        const sunset = document.getElementById("sunset");
+        sunrise.textContent = data.city_info.sunrise;
+        sunset.textContent = data.city_info.sunset;
+
+    } catch (err) { 
+        console.error(err);
+    }
+};
+async function newTown() {
+    const uri = `https://www.prevision-meteo.ch/services/json/toulouse`;
+    try { 
+        const data = await fetchJsonWeatherData(uri);
+        const town = document.getElementById("town");
+        town.textContent = data.city_info.name;
+
+    } catch (err) { 
+        console.error(err);
+    }
+};
+async function newMinMoyMax() {
+    const uri = `https://www.prevision-meteo.ch/services/json/toulouse`;
+    try { 
+        const data = await fetchJsonWeatherData(uri);
+        const minMoyMax = document.getElementById("minMoyMax");
+        let tmoy = (data.fcst_day_0.tmin + data.fcst_day_0.tmax)/2;
+        minMoyMax.innerHTML = `
+            <p>${data.fcst_day_0.tmin}</p>
+            <p>${tmoy}</p>
+            <p>${data.fcst_day_0.tmax}</p>
+        `;
+    } catch (err) { 
+        console.error(err);
+    }
+};
+async function newNextDay() {
+    const uri = `https://www.prevision-meteo.ch/services/json/toulouse`;
+    try { 
+        const data = await fetchJsonWeatherData(uri);
+        const nextDay = document.getElementById("nextDays");
+        for (let i= 2; i<=4; i++) {
+            const previsionsJour = data['fcst_day_' + i]; // on recupère les données du jour selectionné en fonction de i
+
+            const day = document.createElement("div");
+            const icoDay = document.createElement("img");
+            const p = document.createElement("p");
+            const moyTemp = (previsionsJour.tmin + previsionsJour.tmax)/2;
+
+            icoDay.src = previsionsJour.icon;
+            p.textContent = moyTemp;
+
+            day.appendChild(icoDay);
+            day.appendChild(p);
+            nextDay.appendChild(day);
+        }        
+    } catch (err) { 
+        console.error(err);
+    } 
+}
 
 
-                    
-// on doit récupérer l'heure toute les secondes pour afficher seconde par seconde
