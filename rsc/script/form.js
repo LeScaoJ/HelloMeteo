f9500d152accdde0c5feed94bcdb008cfdd4f37e
Julie Le Scao <lescao.julie@gmail.com>
2023-07-11 14:23:51 +0200

    fin du formulaire avec controle JS

--- rsc/script/form.js
+++ rsc/script/form.js
@@ -1,44 +1,48 @@
 // pour gérer la vérification des champs du formulaire
 
-/* Vérification générale du formulaire */
-document.querySelector("form").addEventListener("submit", (evt) => {
-    evt.preventDefault();
-    const nomOk= checkID();
-    const mailOk = checkMail();
-    const subjectOk = checkSubject();
-    const messOk = checkMss();
-    if (nomOk && mailOk && subjectOk && messOk) {
-        alert("votre message a bien été envoyé");
-    } else {
-        alert("les champs ne sont pas bien remplis, votre message n'a pas pu être envoyé");
-    }
-}); 
-
 /* Vérification de chaque contrôle à la frappe */
-document.getElementById("nom").addEventListener("keyup", checkID);
+document.getElementById("firstName").addEventListener("keyup", checkID);
 document.getElementById("mail").addEventListener("keyup", checkMail);
 document.getElementById("subject").addEventListener("keyup", checkSubject);
 document.getElementById("mss").addEventListener("keyup", checkMss);
 
+/* Vérification générale du formulaire */
+document.querySelector("form").addEventListener("submit", (evt) => {
+    const nameOk= checkID();
+    const mailOk = checkMail();
+    const subjectOk = checkSubject();
+    const messOk = checkMss();
+    if (nameOk && mailOk && subjectOk && messOk) {
+        infoText("votre message a bien été envoyé");
+    } else {
+        evt.preventDefault();
+        infoText("les champs ne sont pas bien remplis, votre message n'a pas pu être envoyé");
+    }
+}); 
+
 /* fonctions de vérification */
 function checkID(){
-    const nom = document.getElementById("nom");
-    const value = nom.value.trim();
-    if (value != "") {
+    const firstName = document.getElementById("firstName");
+    const value = firstName.value.trim();
+    const infoText = document.getElementById("pbfirstName");
+    if (value.length != 0) {
+        infoText.classList.remove("show");
         return true;
     } else {
+        infoText.classList.add("show");
         return false;
     }
-    /*je vérifie que le champs n'est pas vide*/
-
 };
 
 function checkMail(){
     const mail = document.getElementById("mail");
     const value = mail.value.trim();
-    if ((value == "") && ((isValidEmail(mail) === true))) { /* A VERIFIER */
+    const infoText = document.getElementById("pbMail");
+    if ((value.length != 0) && ((isValidEmail(value) === true))) { 
+        infoText.classList.remove("show");
         return true;
     } else {
+        infoText.classList.add("show");
         return false;
     }
     /*je vérifie que le champs n'est pas vide et que le texte rempli par l'utilisateur est bien conforme à la regle défini par l'expression régulière choisie */
@@ -47,14 +51,17 @@ function checkMail(){
 function isValidEmail(mail) {
     var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(mail);
-  }
+};
 
 function checkSubject() {
     const subject = document.getElementById("subject");
     const value = subject.value.trim();
-    if (value != "") {
+    const infoText = document.getElementById("pbSubject");
+    if (value.length != 0) {
+        infoText.classList.remove("show");
         return true;
     } else {
+        infoText.classList.add("show");
         return false;
     }
     /*je vérifie que le champs n'est pas vide*/
@@ -64,15 +71,18 @@ function checkSubject() {
 function checkMss(){
     const mess = document.getElementById("mss");
     const value = mess.value.trim();
-    if (value != "") {
+    const infoText = document.getElementById("pbMss");
+    if (value.length != 0) {
+        infoText.classList.remove("show");
         return true;
     } else {
+        infoText.classList.add("show");
         return false;
     }
-    /*je vérifie que le champs n'est pas vide*/
+    /*je vérifie que le champs n'est pas vide et j'affiche un mss en conséquence*/
 
 };
 
 
-/* AJOUT class list ou class name pour afficher les alerts si les champs ne sont pas corerctement rempli*/
+/* AJOUT class list ou class name pour afficher les infoTexts si les champs ne sont pas corerctement rempli*/
 
